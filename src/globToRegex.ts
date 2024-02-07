b22eb7dec3c6cf14b8430e832faf5c8f2b1864a3
BrettOrmsby <brettwormsby@outlook.com>
2024-02-07 11:14:48 -0500

    fix: avoid catastrophic backtracking regex error

--- src/globToRegex.ts
+++ src/globToRegex.ts
@@ -29,14 +29,23 @@ export default function globToRegex(glob: string): RegExp {
       case "*":
         {
           // Check how many stars there are
+          const prevChar = glob[i - 1];
           let numberStars = 1;
-          while (glob.charAt(i + 1) === "*") {
+          while (glob[i + 1] === "*") {
             i += 1;
             numberStars += 1;
           }
-          if (numberStars > 1) {
+          const nextChar = glob[i + 1];
+
+          const isDoubleStar =
+            numberStars > 1 &&
+            (prevChar === "." || prevChar === undefined) &&
+            (nextChar === "." || nextChar === undefined);
+
+          if (isDoubleStar) {
             // Match any number of sections
-            regexString += "((?:[^.]*(?:.|$))*?)";
+            regexString += "((?:[^.]*(?:\\.|$))*?)";
+            i += 1; // skip the next "." character
           } else {
             // Match until a section ends
             regexString += "([^.]*)";
